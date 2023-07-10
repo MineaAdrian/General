2b53388a9397ef4aaafaf1d226ba1ce8cb1596f6
Minea Adrian <minea.adrian94@gmail.com>
2023-07-10 10:23:19 +0300

    Added data validation

--- C#/E-Commerce App ASP.NET Core MVC/Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ C#/E-Commerce App ASP.NET Core MVC/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -22,5 +22,25 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the displayorder cannot exactly match the name.");
+            }
+            //if (obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value.");
+            //}
+            if (ModelState.IsValid) 
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+             return RedirectToAction("Index", "Category");
+            }
+            else { return View(); }
+        }
     }
 }
