9521c61f4dcd478641bb521f6986a17313850c3b
Minea Adrian <minea.adrian94@gmail.com>
2024-01-13 07:04:06 +0200

    Project update

---
+++ PHP/Project/app/views/pages/index.php
@@ -0,0 +1,16 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+
+
+    <?php
+    if (!empty($data)) {
+        echo $data['title'];
+    }
+
+    ?>
+
+<?php //echo URLROOT;
+//// Test on how we can access the app root
+//    echo APPROOT;
+//    echo SITENAME;
+
+require APPROOT . '/views/inc/footer.php';
