ffcfd108f81bd57c904a57e70009765cd65204db
Minea Adrian <minea.adrian94@gmail.com>
2024-01-23 10:49:55 +0200

    navbar edited/Posts controller added

--- PHP/shareposts/app/views/inc/navbar.php
+++ PHP/shareposts/app/views/inc/navbar.php
@@ -1,43 +1,46 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-    <div class="container-fluid" style="margin:auto 12% auto 12%">
+    <div class="container">
         <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarExampleDefault"
-                aria-controls="navbarExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarExampleDefault">
-            <ul class="navbar-nav me-auto ">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <?php if (isset($_SESSION['user_id'])) : ?>
-
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page"
-                           href="<?php echo URLROOT; ?>/users/logout">Logout</a>
-                    </li>
-
-                <?php else : ?>
-
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page"
-                           href="<?php echo URLROOT; ?>/users/register">Register</a>
+                        <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Log In</a>
+                        <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
                     </li>
-                <?php endif; ?>
-            </ul>
-            <form class="d-flex" role="search" style="margin: auto 2% auto 2%">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <?php if (isset($_SESSION['user_id'])) : ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page"
+                               href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+                        </li>
+
+                    <?php else : ?>
+
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page"
+                               href="<?php echo URLROOT; ?>/users/register">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Log In</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
     </div>
 </nav>
+
